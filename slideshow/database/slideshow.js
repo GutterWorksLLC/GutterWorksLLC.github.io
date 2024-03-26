1cfc04f05441b3fe1429be1fe0d97217487d0654
GutterWorksLLC <gworks209@protonmail.com>
2024-03-25 20:41:52 -0700

    initial commit

---
+++ slideshow/database/slideshow.js
@@ -0,0 +1,44 @@
+var slideshow = 
+[
+	// Position:
+	//
+	// 	^	50 (High)
+	//	|	45
+	//	|	40
+	//	|	35 
+	//	|	30
+	//	|	25 (Default)
+	//	|	20
+	//	|	15
+	//	|	10
+	//	|	5
+	//	v	0  (Baseline)
+
+	// ----------------------------------------
+	// Slides
+	// ----------------------------------------
+
+	[	/* Title */		"01",
+		/* Image */		"01.webp",
+		/* Position */		"2",
+		/* Link */		"https://gutterworks.github.io"
+	],
+
+	// ----------------------------------------
+
+	[	
+		/* Title */		"02",
+		/* Image */		"02.webp",
+		/* Position */		"8",
+		/* Link */		"https://gutterworks.github.io"
+	],
+
+	// ----------------------------------------
+
+	[	
+		/* Title */		"03",
+		/* Image */		"03.webp",
+		/* Position */		"10",
+		/* Link */		"https://gutterworks.github.io"
+	],
+];
